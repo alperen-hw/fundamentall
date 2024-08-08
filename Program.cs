b33a846778fb972e4d975497b8daed08d242e60a
alperen-hw <lilithiaxoxo@gmail.com>
2024-08-08 21:58:27 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace FirstConsole
+{
+   
+
+    internal class Program
+    { 
+        
+        
+        static void Main(string[] args)
+        {
+
+            #region Degisken Isimlendirme Yazim Prensipleri
+
+            // Pascal Case => WriteLine
+            // camel case => writeLine
+            // snake case => write_line
+            // kebap case =  write-line
+            #endregion
+
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, Mars!");
+        }
+        
+    }
+   
+}
+
+
